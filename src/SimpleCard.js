b09f5d1648f7f515df99214e8203392464cf89d8
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2023-11-23 10:14:34 -1200

    Added some html and css

--- src/SimpleCard.js
+++ src/SimpleCard.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
-import Title from "./Title";
-import Description from "./Description";
-import Image from "./Imag";
+import React from 'react';
+import Title from './Title';
+import Description from './Description';
+import Image from './Imag';
+import '../src/simplecard.css'; // Import the CSS file
 
-class SimpleCard extends Component {
-  render() {
-    const { title, description, imageUrl } = this.props;
-
-    return (
-      <div className="simple-card">
-        <Title text={title} />
-        <Description text={description} />
-        <Image url={imageUrl} />
+const SimpleCard = () => {
+  return (
+    <div className="simple-card">
+      <div className="text1">
+        <div className="img1">
+          <Image url="pics/frontpage.png" alt="" width="180" />
+        </div>
+        <Title text="Billy Gates" /> {/* Use the Title component */}
+        <h2 className="text-h2">Amazoni su</h2>
+        <Description text="I am Amazon su" /> {/* Use the Description component */}
+        <p className="text-p2">
+          Lorem ipsum dolor,sit amet consectetur adip isicing elit.
+        </p>
+        <button className="btnn">HOWDIY</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SimpleCard;
